853a448ad0ae3fa1ad7702c116b96b5fd9d84f59
AngryBearCode <angrybearcode@gmail.com>
2018-09-27 19:15:44 -0400

    Initial Commit

---
+++ HomeWorkerClassGen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWorkerClassGen
+//
+//  Created by Ryan Topham on 9/27/18.
+//  Copyright © 2018 Angry Bear Coding Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
